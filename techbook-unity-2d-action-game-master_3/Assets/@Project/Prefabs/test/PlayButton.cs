de07767444b0f9e790668ac12d189cc289811f72
MFbuider <62496746+MFbuider@users.noreply.github.com>
2021-02-06 20:06:57 +0800

    Jungle Adventure
    Jungle Adventure

---
+++ techbook-unity-2d-action-game-master_3/Assets/@Project/Prefabs/test/PlayButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PlayButton : MonoBehaviour, IPointerEnterHandler
+{
+    private Button pb;
+    public Sprite Sprite;
+
+    private void Start()
+    {
+        pb = GetComponent<Button>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+
+    }
+}
